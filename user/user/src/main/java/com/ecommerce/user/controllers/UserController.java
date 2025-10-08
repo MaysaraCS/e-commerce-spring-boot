a1ebe4333ebacc1f3aa973dce8d00095c06b14e3
Maysara <maysaracs1001@gmail.com>
2025-10-08 12:59:22 +0300

    check logs

--- user/user/src/main/java/com/ecommerce/user/controllers/UserController.java
+++ user/user/src/main/java/com/ecommerce/user/controllers/UserController.java
@@ -3,6 +3,9 @@ import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +15,11 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
+    //private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers(){
@@ -22,6 +27,19 @@ public class UserController {
     }
     @GetMapping("/{id}")
     public ResponseEntity <UserResponse> getUser(@PathVariable String id){
+//        logger.info("Fetching user with id {}", id);
+//        logger.trace("Fetching user with id {}", id);
+//        logger.debug("Fetching user with id {}", id);
+//        logger.info("Fetching user with id {}", id);
+//        logger.warn("Fetching user with id {}", id);
+//        logger.error("Fetching user with id {}", id);
+        log.info("Fetching user with id {}", id);
+        log.trace("Fetching user with id {}", id);
+        log.debug("Fetching user with id {}", id);
+        log.info("Fetching user with id {}", id);
+        log.warn("Fetching user with id {}", id);
+        log.error("Fetching user with id {}", id);
+
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
