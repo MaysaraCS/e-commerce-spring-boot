6184b2c6fedc695a376b7b7e0987fdfa82794194
Maysara <maysaracs1001@gmail.com>
2025-10-12 17:37:57 +0300

    test rabbitMQ

--- order/order/src/main/java/com/ecommerce/order/services/OrderService.java
+++ order/order/src/main/java/com/ecommerce/order/services/OrderService.java
@@ -1,46 +1,55 @@
 package com.ecommerce.order.services;
 
+import com.ecommerce.order.dto.OrderCreatedEvent;
+import com.ecommerce.order.repository.OrderRepository;
+import com.ecommerce.order.models.OrderStatus;
 import com.ecommerce.order.dto.OrderItemDTO;
 import com.ecommerce.order.dto.OrderResponse;
-import com.ecommerce.order.models.Order;
 import com.ecommerce.order.models.CartItem;
+import com.ecommerce.order.models.Order;
 import com.ecommerce.order.models.OrderItem;
-import com.ecommerce.order.models.OrderStatus;
-import com.ecommerce.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final CartServices cartService;
     private final OrderRepository orderRepository;
+    private final StreamBridge streamBridge;
 
     public Optional<OrderResponse> createOrder(String userId) {
-        // validate for cart items
+        // Validate for cart items
         List<CartItem> cartItems = cartService.getCart(userId);
-        if(cartItems.isEmpty()){
+        if (cartItems.isEmpty()) {
             return Optional.empty();
         }
-//        // validate for user
-//        Optional<User> userOptional=  userRepository.findById(Long.valueOf(userId));
-//        if(userOptional.isEmpty()){
+//        // Validate for user
+//
+//        Optional<User> userOptional = userRepository.findById(Long.valueOf(userId));
+//        if (userOptional.isEmpty()) {
 //            return Optional.empty();
 //        }
 //        User user = userOptional.get();
-        // calculate total price
+
+        // Calculate total price
         BigDecimal totalPrice = cartItems.stream()
                 .map(CartItem::getPrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
-        // create order
+
+        // Create order
         Order order = new Order();
         order.setUserId(userId);
         order.setStatus(OrderStatus.CONFIRMED);
         order.setTotalAmount(totalPrice);
+
         List<OrderItem> orderItems = cartItems.stream()
                 .map(item -> new OrderItem(
                         null,
@@ -50,11 +59,36 @@ public class OrderService {
                         order
                 ))
                 .toList();
+
         order.setItems(orderItems);
         Order savedOrder = orderRepository.save(order);
-        // clear the cart
+
+        // Clear the cart
         cartService.clearCart(userId);
+
+        // Publish order created event
+        OrderCreatedEvent event = new OrderCreatedEvent(
+                savedOrder.getId(),
+                savedOrder.getUserId(),
+                savedOrder.getStatus(),
+                mapToOrderItemDTOs(savedOrder.getItems()),
+                savedOrder.getTotalAmount(),
+                savedOrder.getCreatedAt()
+        );
+        streamBridge.send("createOrder-out-0", event);
+
         return Optional.of(mapToOrderResponse(savedOrder));
+    }
+
+    private List<OrderItemDTO> mapToOrderItemDTOs(List<OrderItem> items) {
+        return items.stream()
+                .map(item -> new OrderItemDTO(
+                        item.getId(),
+                        item.getProductId(),
+                        item.getQuantity(),
+                        item.getPrice(),
+                        item.getPrice().multiply(new BigDecimal(item.getQuantity()))
+                )).collect(Collectors.toList());
     }
 
     private OrderResponse mapToOrderResponse(Order order) {
